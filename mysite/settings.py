f7eedac4c191b214ee3af692a3df343e8443bd3f
ashkan00700 <behnamhoshmand.torch@gmail.com>
2025-07-11 20:12:46 +0330

    تنظیم نهایی DATABASES و STATIC برای Render

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,8 +1,13 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+DATABASES = {
+    "default": dj_database_url.config(default="sqlite:///db.sqlite3")
+}
