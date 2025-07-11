021807c80d471373ace3da59a0926ecd4d64cd2f
ashkan00700 <behnamhoshmand.torch@gmail.com>
2025-07-11 20:21:27 +0330

    حذف collectstatic برای جلوگیری از خطای Render

--- build.sh
+++ build.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
-# نصب وابستگی‌ها
-pip install -r requirements.txt
-
-# اجرای migrate برای دیتابیس
+# اجرای migrate برای اعمال تغییرات دیتابیس
 python manage.py migrate
-
-# جمع‌آوری static files برای سرویس‌دهی در production
-python manage.py collectstatic --noinput
