bb1b77eb2bad2c5021549fb97b292e16b8d90162
ashkan00700 <behnamhoshmand.torch@gmail.com>
2025-07-11 20:04:38 +0330

    تعریف BASE_DIR و تنظیمات STATIC برای رفع ارور Render

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,6 +1,7 @@
 import os
+from pathlib import Path
 
-# سایر تنظیمات شما...
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
